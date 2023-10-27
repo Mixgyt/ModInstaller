04771e1c79efd1db1007fafff1a1f47b411b1e16
Mixgyt <gmix990@gmail.com>
2023-10-27 11:32:38 -0600

    Cambios en el lector json

--- utils/json_util.py
+++ utils/json_util.py
@@ -6,8 +6,20 @@ def jsonReader(fileName:str):
     try:
         with(fileName,"r") as file:
             data = json.load(file)
+            
     except(FileExistsError,FileNotFoundError):
         print("Error de nombre de archivo")
         return data
     
     return data
+
+def jsonCreate(fileName:str):
+    try:
+        with(fileName,"w") as file:
+            json.dump("",file,indent="\t")
+            
+    except():
+        print("Error al crear json")
+        return False
+    
+    return True
